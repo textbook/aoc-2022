9dbd17fce7fbb101f0c905d2bbfca52a46ad354a
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-12-07 08:48:54 +0000

    Day 7 part 1

---
+++ day07/index.test.ts
@@ -0,0 +1,53 @@
+import { describe, it } from "bun:test";
+import { expect } from "chai";
+
+import { parser, solution } from "./index";
+
+describe("day 07", () => {
+	it("works for a simple example", () => {
+		expect(solution(simpleExample)).to.equal(95_437);
+	});
+
+	describe("parser", () => {
+		it("creates a simple file tree", () => {
+			expect(parser("$ cd /\n$ls\ndir a\n123 b.txt"))
+				.to.deep.equal({ "a": {}, "b.txt": 123 });
+		});
+
+		it("can move down the file system", () => {
+			expect(parser("$ cd /\n$ ls\ndir a\n$ cd a\n$ ls\n123 b.txt"))
+				.to.deep.equal({ a: { "b.txt": 123 } });
+		});
+
+		it("can move up the file system", () => {
+			expect(parser("$ cd /\n$ ls\ndir a\n$ cd a\n$ cd ..\n$ ls\n123 b.txt"))
+				.to.deep.equal({ a: {}, "b.txt": 123 });
+		});
+	});
+});
+
+const simpleExample = `
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+`.trim();
---
+++ day07/index.ts
@@ -0,0 +1,65 @@
+import { sum } from "../utils";
+
+interface Directory {
+	[key: string]: Directory | number;
+}
+
+export const solution: Solution = (input: string): number => {
+	const tree = parser(input);
+	const sizes = getSizes(tree, {});
+	return sum(Object.values(sizes).filter((size) => size <= 100_000));
+};
+
+const getSizes = (dir: Directory, sizes: Record<string, number>, path = ""): Record<string, number> => {
+	Object.entries(dir).forEach(([name, content]) => {
+		if (typeof content === "number") {
+			path.split("/").forEach((_, index, parts) => {
+				const key = parts.slice(0, index + 1).join("/") || "/";
+				sizes[key] = (sizes[key] ?? 0) + content;
+			});
+		} else {
+			getSizes(content, sizes, `${path}/${name}`);
+		}
+	});
+	return sizes;
+};
+
+export const parser = (input: string): Directory => {
+	const tree = {} as Directory;
+	let path: string = "$";
+
+	input.trim().split("\n").forEach((line) => {
+		if (line.startsWith("$ cd")) {
+			const [, , dir] = line.split(" ");
+			path = updatePath(path, dir);
+		} else if (!line.startsWith("$")) {
+			const [meta, name] = line.split(" ");
+			if (meta === "dir") {
+				addItem(tree, path, name, {});
+			} else {
+				addItem(tree, path, name, parseInt(meta, 10));
+			}
+		}
+	});
+
+	return tree;
+};
+
+const addItem = (tree: Directory, path: string, name: string, item: Directory | number): void => {
+	let dir = tree;
+	path.split(".").slice(1).forEach((dirname) => {
+		dir = dir[dirname] as Directory;
+	});
+	dir[name] = item;
+}
+
+const updatePath = (path: string, dir: string) => {
+	switch (dir) {
+		case "/":
+			return "$";
+		case "..":
+			return path.split(".").slice(0, -1).join(".");
+		default:
+			return `${path}.${dir}`;
+	}
+};
