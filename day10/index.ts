4494a4a29420b7ce0412efe06f61c6aad0aa61bf
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-12-10 08:18:21 +0000

    Day 10 part 2

--- day10/index.ts
+++ day10/index.ts
@@ -1,27 +1,27 @@
-import { sum } from "../utils";
-
-const MEASURED = [...new Array(6)].map((_, index) => 20 + (40 * index));
-
-export const solution: Solution = (input: string): number => {
+export const solution: Solution = (input: string): string => {
 	let X = 1;
-	const signalStrengths: number[] = [];
+	const screen = [...new Array(6)].map(() => [...new Array<string>(40)].fill("."));
 	const instructions = parse(input);
 
-	for (let cycle = 1; cycle <= 220; cycle++) {
-		if (MEASURED.includes(cycle)) {
-			signalStrengths.push(cycle * X);
-		}
+	for (let cycle = 1; cycle <= 240; cycle++) {
+		draw(X, cycle, screen);
 		const instruction = instructions.shift();
 		if (instruction.name === "addx") {
 			cycle++;
-			if (MEASURED.includes(cycle)) {
-				signalStrengths.push(cycle * X);
-			}
+			draw(X, cycle, screen);
 			X += instruction.value;
 		}
 	}
 
-	return sum(signalStrengths);
+	return screen.map((row) => row.join("")).join("\n");
+};
+
+const draw = (X: number, cycle: number, screen: string[][]) => {
+	const x = (cycle - 1) % 40;
+	const y = Math.floor((cycle - 1) / 40);
+	if (X - 1 <= x && x <= X + 1) {
+		screen[y][x] = "#";
+	}
 };
 
 type AddX = { name: "addx", value: number };
