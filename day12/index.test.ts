4bb4ddee38269a43b4b63c0a118c649c9dc96d1c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-12-12 08:41:49 +0000

    Day 12 part 1

---
+++ day12/index.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from "bun:test";
+
+import { parser, solution } from "./index";
+
+describe("day 12", () => {
+	it("works for a simple example", () => {
+		expect(solution(simpleExample)).toBe(31);
+	});
+
+	describe("parser", () => {
+		it("finds the start", () => {
+			const { start } = parser(simpleExample);
+			expect(start).toStrictEqual([0, 0]);
+		});
+
+		it("finds the end", () => {
+			const { end } = parser(simpleExample);
+			expect(end).toStrictEqual([5, 2]);
+		});
+
+		it("maps the terrain", () => {
+			const { terrain } = parser(simpleExample);
+			expect(terrain).toStrictEqual([
+				[ 0,  0,  1, 16, 15, 14, 13, 12],
+				[ 0,  1,  2, 17, 24, 23, 23, 11],
+				[ 0,  2,  2, 18, 25, 25, 23, 10],
+				[ 0,  2,  2, 19, 20, 21, 22,  9],
+				[ 0,  1,  3,  4,  5,  6,  7,  8],
+			]);
+		});
+	});
+});
+
+const simpleExample = `
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+`;
---
+++ day12/index.ts
@@ -0,0 +1,65 @@
+type Terrain = number[][];
+type Position = [number, number];
+
+export const solution: Solution = (input: string): number => {
+	const { start, end, terrain } = parser(input);
+	const path = findPath(terrain, start, end);
+	return path.length - 1;
+};
+
+const findPath = (terrain: Terrain, from: Position, to: Position): string[] => {
+	let paths: string[][] = [[from.toString()]];
+	while (true) {
+		paths = paths
+			.flatMap((path) => validMoves(fromString(path.at(-1)), terrain)
+				.map((position) => position.toString())
+				.filter((position) => !path.includes(position))
+				.map((position) => [...path, position]));
+		paths = Object.values(Object.fromEntries(paths.map((path) => [path.at(-1), path])));
+		for (const path of paths) {
+			const [x, y] = fromString(path.at(-1));
+			if (x === to[0] && y === to[1]) {
+				return path;
+			}
+		}
+	}
+};
+
+const fromString = (saved: string): Position => {
+	const [x, y] = saved.split(",");
+	return [parseInt(x, 10), parseInt(y, 10)];
+}
+
+const MOVES = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+
+const validMoves = ([x, y]: Position, terrain: Terrain): Position[] => MOVES
+	.map(([dx, dy]): Position => [x + dx, y + dy])
+	.filter(([new_x, new_y]) => {
+		return new_x >= 0
+			&& new_x < terrain[0].length
+			&& new_y >= 0
+			&& new_y < terrain.length
+			&& terrain[new_y][new_x] <= (terrain[y][x] + 1);
+	});
+
+const a = "a".charCodeAt(0);
+
+export const parser = (input: string): { terrain: Terrain, start: Position, end: Position } => {
+	let start, end;
+	const terrain = input
+		.trim()
+		.split("\n")
+		.map((row, y) => row
+			.split("")
+			.map((cell, x) => {
+				if (cell === "S") {
+					start = [x, y];
+					cell = "a";
+				} else if (cell === "E") {
+					end = [x, y];
+					cell = "z";
+				}
+				return cell.charCodeAt(0) - a;
+			}));
+	return { end, start, terrain };
+};
