0b959ea01f7fd6c22d5c3fc0e1d5b4d3a01ee7c5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-12-13 09:05:03 +0000

    Refactor

--- day13/index.ts
+++ day13/index.ts
@@ -2,49 +2,53 @@ import { product } from "../utils";
 
 type Packet = Array<number | Packet>;
 
-const DIVIDERS = ["[[2]]", "[[6]]"]
+const DIVIDERS: Packet[] = [[[2]], [[6]]];
 
 export const solution: Solution = (input: string): number => {
-	const packets: Packet[] = input
-		.trim()
-		.split("\n\n")
-		.flatMap((pair): [Packet, Packet] => {
-			const [left, right] = pair.split("\n");
-			return [JSON.parse(left), JSON.parse(right)];
-		});
-	packets.push(...DIVIDERS.map((packet): Packet => JSON.parse(packet)));
-	packets.sort((left, right): -1 | 0 | 1 => {
-		switch (valid(left, right)) {
-			case true:
-				return -1;
-			case false:
-				return 1;
-			default:
-				return 0;
-		}
-	});
+	const packets: Packet[] = parse(input);
+	packets.push(...DIVIDERS);
+	packets.sort(smallestFirst);
 	return product(packets.map((packet, index) => {
-		return DIVIDERS.includes(JSON.stringify(packet)) ? index + 1 : 1;
+		return DIVIDERS.includes(packet) ? index + 1 : 1;
 	}));
 };
 
-export const valid = (left: Packet | number, right: Packet | number): boolean | null => {
+const smallestFirst = (left: Packet, right: Packet): -1 | 0 | 1 => {
+	switch (leftSmaller(left, right)) {
+		case true:
+			return -1;
+		case false:
+			return 1;
+		default:
+			return 0;
+	}
+};
+
+export const leftSmaller = (left: Packet | number, right: Packet | number): boolean | null => {
 	if (Array.isArray(left) && Array.isArray(right)) {
 		for (let index = 0; index < left.length; index++) {
 			if (index >= right.length) {
 				return false;
 			}
-			const isValid = valid(left[index], right[index]);
+			const isValid = leftSmaller(left[index], right[index]);
 			if (isValid !== null) {
 				return isValid;
 			}
 		}
 		return left.length < right.length || null;
 	} else if (Array.isArray(left)) {
-		return valid(left, [right]);
+		return leftSmaller(left, [right]);
 	} else if (Array.isArray(right)) {
-		return valid([left], right);
+		return leftSmaller([left], right);
 	} else {
 		return (left === right) ? null : (left < right);
 	}
 };
+
+const parse = (input: string): Packet[] => input
+	.trim()
+	.split("\n\n")
+	.flatMap((pair): [Packet, Packet] => {
+		const [left, right] = pair.split("\n");
+		return [JSON.parse(left), JSON.parse(right)];
+	});
